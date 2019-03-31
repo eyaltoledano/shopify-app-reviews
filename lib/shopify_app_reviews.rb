b3d96a216169d08a8774e36a7bd09610617358c3
eyaltoledano <eyaltoledano@me.com>
2019-03-31 14:06:17 -0400

    Initial Commit

---
+++ lib/shopify_app_reviews.rb
@@ -0,0 +1,6 @@
+require "shopify_app_reviews/version"
+
+module ShopifyAppReviews
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shopify_app_reviews/version.rb
@@ -0,0 +1,3 @@
+module ShopifyAppReviews
+  VERSION = "0.1.0"
+end
