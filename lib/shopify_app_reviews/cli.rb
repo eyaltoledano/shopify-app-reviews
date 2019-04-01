12f59b3be6cd439f5a0a49724a00148b2baadb33
eyaltoledano <eyaltoledano@me.com>
2019-04-01 12:07:05 -0400

    Added some feedback while the app loads Shopify apps and returns the time the Library was updated.

--- lib/shopify_app_reviews/cli.rb
+++ lib/shopify_app_reviews/cli.rb
@@ -10,46 +10,51 @@ class ShopifyAppReviews::CLI
     welcome
     scrape_and_create_apps
     # add_reviews_to_apps
+    now = Time.now.asctime
+    puts " Done."
+    puts "Library updated as of #{now}."
     get_input
     goodbye
   end
 
   def welcome
-    puts "The Shopify App Library is being updated. One moment."
+    print "The Shopify App Library is being updated. One moment."
   end
 
   def get_input
     input = nil
-    while input != "exit"
+    while input != "exit cli"
       puts "Search for a Shopify app by name or URL to access its information."
-      puts "You can use 'exit' to leave at any time."
+      puts "You can use 'exit cli' to leave at any time."
       print "Please enter the name or URL of a Shopify app: "
       input = gets.chomp.downcase
-      display_app_details(input) ? display_app_details(input) : puts("Doesn't look like that app exists. Did you spell that right?")
+      display_app_details(input) ? display_app_details(input) : puts("Doesn't look like that app exists. Did you spell that right?") unless input == "exit cli"
     end
   end
 
   def display_app_details(input) # if an app is not found, return falsey
-    requested_app = ShopifyApp.find_by_url(input)
-    requested_app = ShopifyApp.find_by_name(input) if requested_app.nil?
-    requested_app.nil? ? false : app_table(requested_app)
+    unless input == "exit cli"
+      requested_app = ShopifyApp.find_by_url(input)
+      requested_app = ShopifyApp.find_by_name(input) if requested_app.nil?
+      requested_app.nil? ? false : app_table(requested_app)
 
-    unless false
-      sub_input = nil
-      while !sub_input != "new app"
-        puts "Use 'show reviews' to see #{requested_app.name}'s reviews.'"
-        puts "Use 'back to app' to review #{requested_app.name}'s details.'"
-        puts "Use 'new app' to return to the previous menu."
-        sub_input = gets.chomp.downcase
-        if sub_input == "show reviews"
-          reviews_table(requested_app)
+      unless false
+        sub_input = nil
+        while !sub_input != "new app"
+          puts "Use 'show reviews' to see #{requested_app.name}'s reviews.'"
+          puts "Use 'back to app' to review #{requested_app.name}'s details.'"
+          puts "Use 'new app' to return to the previous menu."
+          sub_input = gets.chomp.downcase
+          if sub_input == "show reviews"
+            reviews_table(requested_app)
+          end
+          get_input if sub_input == "new app"
+          if sub_input == "exit" || sub_input == "quit"
+            goodbye
+            exit
+          end
+          app_table(requested_app) if sub_input == "back to app"
         end
-        get_input if sub_input == "new app"
-        if sub_input == "exit" || sub_input == "quit"
-          goodbye
-          exit
-        end
-        app_table(requested_app) if sub_input == "back to app"
       end
     end
   end
