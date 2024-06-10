0bba96b0eada50f4a15e2e5173d5ed484513545c
Eyal <eutait@gmail.com>
2024-06-10 13:46:13 -0400

    Fixes the app, makes it only scrape and save to CSV one app at a time.

--- lib/shopify_app_reviews/app_review_scraper.rb
+++ lib/shopify_app_reviews/app_review_scraper.rb
@@ -1,36 +1,111 @@
 require 'open-uri'
 require 'nokogiri'
+require 'csv'
 require 'pry'
+require 'pp'
 
 class AppReviewScraper
-  def self.scrape_app_reviews(app) # returns an array of AppReview hashes
-    html = open(app.url+"/reviews")
-    index = Nokogiri::HTML(html)
-    review_array = []
-    index.css('.review-listing').each do |review|
-      hash = {}
-      hash[:title] = review.css('.review-listing-header__text').text.strip
-      hash[:body] = review.css('.review-content').css('p').text
-      hash[:date] = review.css('.review-metadata__item')[1].css('div').children.last.text.strip
-      hash[:rating] = review.css('.ui-star-rating').css('div.ui-star-rating__text').text
-      hash[:app] = app
-      review_array << hash
+  BASE_URL = "https://apps.shopify.com"
+
+  def self.scrape_app(slug)
+    url = "#{BASE_URL}/#{slug}"
+    html = URI.open(url)
+    doc = Nokogiri::HTML4(html)
+
+    # Attempt to find the review summary element through iteration
+    review_summary = nil
+    doc.css('[data-truncate-app-review-summary]').each do |element|
+      if element.at_css('p[data-truncate-content-copy]')
+        review_summary = element.at_css('p[data-truncate-content-copy]').text.strip
+        break
+      end
     end
-    review_array
+
+    review_summary = review_summary || "No summary available"
+    
+    app_data = {
+      name: slug,
+      url: url,
+      review_summary: review_summary
+    }
+
+    puts "Slug: #{app_data[:name]}"
+    puts "URL: #{app_data[:url]}"
+    puts "Review Summary:\n#{app_data[:review_summary]}"
+
+    app_data
   end
 
-  def self.scrape_app_metadata(app)
-    html = open(app.url+"/reviews")
-    index = Nokogiri::HTML(html)
-    app.description = index.css('.ui-app-store-hero__description-text').text
-    overall_rating = index.css('.ui-star-rating__rating').first.text
-    app.overall_rating = overall_rating
 
-    html2 = open(app.url)
-    index2 = Nokogiri::HTML(html2)
-    dev_contact = index2.css('li.app-support-list__item span').children.find do |li|
-      li.text.strip if li.text.include?("@")
+  def self.scrape_app_reviews(slug)
+    url = "#{BASE_URL}/#{slug}/reviews"
+    html = URI.open(url)
+    doc = Nokogiri::HTML4(html)
+
+    total_reviews_text = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/h2/span[2]')&.text.strip.gsub(/[()]/, '')
+    total_reviews = total_reviews_text.to_i
+    total_pages = (total_reviews / 10.0).ceil
+    rating = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[2]/div[1]').text.strip
+    five_star_reviews = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[1]/div[3]/a/span').text.strip
+    four_star_reviews = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[2]/div[3]/a/span').text.strip
+    three_star_reviews = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[3]/div[3]/a/span').text.strip
+    two_star_reviews = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[4]/div[3]/a/span').text.strip
+    one_star_reviews = doc.at_xpath('//*[@id="arp-reviews"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[5]/div[3]/a/span').text.strip
+
+    puts "Rating: #{rating} out of 5 stars"
+    puts "5 Star Reviews: #{five_star_reviews}"
+    puts "4 Star Reviews: #{four_star_reviews}"
+    puts "3 Star Reviews: #{three_star_reviews}"
+    puts "2 Star Reviews: #{two_star_reviews}"
+    puts "1 Star Reviews: #{one_star_reviews}"
+    puts "Total Reviews: #{total_reviews}"
+    puts "Total Pages: #{total_pages}"
+    reviews = []
+    (1..total_pages).each do |page_number|
+      reviews += scrape_reviews_page(slug, page_number)
+      puts "Total reviews scraped so far: #{reviews.length}"
+      puts "Waiting 2 seconds"
+      sleep(2) # Wait for 1 second before scraping the next page
     end
-    app.developer_contact = dev_contact.text
+
+    reviews
+  end
+
+  def self.scrape_reviews_page(slug, page_number)
+    puts "Scraping page #{page_number}..."
+    review_number = 1
+    url = "#{BASE_URL}/#{slug}/reviews?page=#{page_number}"
+    html = URI.open(url)
+    doc = Nokogiri::HTML(html)
+
+    reviews = []
+    doc.css('div.tw-pb-md.md\\:tw-pb-lg.tw-mb-md.md\\:tw-mb-lg.tw-pt-0.last\\:tw-pb-0').each do |review|
+      puts "Scraping review #{review_number}"
+      review_number += 1
+      rating_element = review.css('div[aria-label*="out of 5 stars"]').first
+      rating = rating_element ? rating_element['aria-label'].split(" out of 5 stars").first.to_i : 0
+      
+      date_element = review.at_css('div.tw-text-body-xs.tw-text-fg-tertiary')
+      date = date_element ? date_element.text.strip : "No date available"
+      
+      body_element = review.at_css('div[data-truncate-review] p.tw-break-words')
+      body = body_element ? body_element.text.strip : "No body available"
+      
+      store_info_element = review.at_css('div.tw-order-2.lg\\:tw-order-1.lg\\:tw-row-span-2.tw-mt-md.md\\:tw-mt-0.tw-space-y-1.md\\:tw-space-y-2.tw-text-fg-tertiary.tw-text-body-xs')
+      store_name = store_info_element.at_css('div.tw-text-heading-xs.tw-text-fg-primary') ? store_info_element.at_css('div.tw-text-heading-xs.tw-text-fg-primary').text.strip : "No store name available"
+      location = store_info_element.css('div')[1] ? store_info_element.css('div')[1].text.strip : "No location available"
+      time_spent_with_app = store_info_element.css('div')[2] ? store_info_element.css('div')[2].text.strip : "No time spent information available"
+
+      reviews << {
+        rating: rating,
+        date: date,
+        body: body,
+        store_name: store_name,
+        location: location,
+        time_spent_with_app: time_spent_with_app
+      }
+    end
+
+    reviews
   end
 end
--- lib/shopify_app_reviews/cli.rb
+++ lib/shopify_app_reviews/cli.rb
@@ -1,157 +1,94 @@
-# # CLI Controller
-require_relative 'app_review.rb'
-require_relative 'shopify_app.rb'
-require_relative 'app_list_scraper.rb'
-require_relative 'app_review_scraper.rb'
-
+# require_relative 'shopify_app'
+# require_relative 'app_review'
+require_relative 'app_review_scraper'
 require 'colorize'
 
 class ShopifyAppReviews::CLI
   def run
     welcome
-    scrape_and_create_apps
-    library_updated
     get_input
     goodbye
   end
 
   def welcome
     hr
-    print "Welcome to Shopify App Reviews CLI.".colorize(:light_green)
-    print " The Shopify App Library is updating. One moment...".colorize(:green)
+    puts "Welcome to Shopify App Reviews CLI.".colorize(:light_green)
   end
 
   def hr
-    80.times {print "-".colorize(:cyan)}
+    80.times { print "-".colorize(:cyan) }
     puts "-".colorize(:cyan)
   end
 
-  def library_updated
-    now = Time.now.asctime
-    checkmark = "\u2713"
-    puts checkmark.encode('utf-8').colorize(:light_green)
-    puts "Library updated as of #{now}".colorize(:green)
-    hr
-  end
-
   def print_instructions
-    puts "You can use 'exit cli' to leave at any time.".colorize(:yellow)
-    puts "Search for a Shopify app by name or URL to access its information.".colorize(:green)
-    print "Enter the name or URL of a Shopify app: ".colorize(:green)
-  end
-
-  def print_sub_instructions(requested_app)
-    puts "Use 'latest reviews' to see #{requested_app.name}'s latest reviews.".colorize(:yellow)
-    puts "Use 'app details' to review #{requested_app.name}'s details.".colorize(:yellow)
-    puts "Use 'overall sentiment' to see how people feel about #{requested_app.name} in general.".colorize(:yellow)
-    unless requested_app.app_reviews.empty?
-      puts "Use 'trending sentiment' to see how people feel about #{requested_app.name} lately.".colorize(:yellow)
-    end
-    puts "Use 'new app' to return to the previous menu.".colorize(:yellow)
-    puts "You can use 'exit cli' to leave at any time.".colorize(:yellow)
-    print "What would you like to do? ".colorize(:green)
+    puts "You can use 'exit' to leave at any time.".colorize(:yellow)
+    print "Enter the slug of a Shopify app (e.g., 'reconcilely'): ".colorize(:green)
   end
 
   def get_input
     input = nil
-    while input != "exit cli"
+    while input != "exit"
       print_instructions
       input = gets.chomp.downcase
-      display_app_details(input) ? display_app_details(input) : puts("Doesn't look like an app exists for that. Did you spell your request properly?").colorize(:red) unless input == "exit cli"
-    end
-  end
-
-  def display_app_details(input) # if an app is not found, return falsey
-    unless input == "exit cli"
-      requested_app = ShopifyApp.find_by_url(input)
-      requested_app = ShopifyApp.find_by_name(input) if requested_app.nil?
-      requested_app.nil? ? false : app_details_table(requested_app)
-
-      unless false
-        sub_input = nil
-        while !sub_input != "new app"
-          hr
-          if requested_app.nil?
-            puts "Doesn't look like an app exists for that. Did you spell your request properly?".colorize(:red)
-            hr
-            get_input
-          else
-            print_sub_instructions(requested_app)
-          end
-          sub_input = gets.chomp.downcase
-          if sub_input == "latest reviews"
-            display_app_reviews(requested_app)
-          end
-          get_input if sub_input == "new app"
-          if sub_input == "exit" || sub_input == "quit" || sub_input == "exit cli"
-            goodbye
-            exit
-          end
-          app_details_table(requested_app) if sub_input == "app details"
-          overall_sentiment(requested_app) if sub_input == "overall sentiment"
-          unless requested_app.app_reviews.empty?
-            if sub_input == "trending sentiment"
-              hr
-              trending_sentiment(requested_app)
-            end
-          end
-
+      unless input == "exit"
+        begin
+          scrape_and_save_reviews(input)
+        rescue OpenURI::HTTPError => e
+          puts "Failed to retrieve app data: #{e.message}".colorize(:red)
+        rescue StandardError => e
+          puts "An error occurred: #{e.message}".colorize(:red)
         end
       end
     end
   end
 
-  def overall_sentiment(app)
-    hr
-    puts "The overall sentiment for".colorize(:green) + " #{app.name.colorize(:white)} " + "is ".colorize(:green) + "#{app.overall_sentiment}"
+  def scrape_and_save_reviews(slug)
+    app_data = AppReviewScraper.scrape_app(slug)
+    puts app_data
+    app = ShopifyApp.new(app_data)
+
+    puts "Scraping reviews for #{app.name}..."
+    reviews = AppReviewScraper.scrape_app_reviews(slug)
+    app.app_reviews = reviews.map { |review_data| AppReview.new(review_data) }
+
+    save_reviews_to_csv(app)
   end
 
-  def trending_sentiment(app)
-    puts "The trending sentiment for".colorize(:green) + " #{app.name.colorize(:white)} " + "is ".colorize(:green) + "#{app.trending_sentiment}"
-  end
-
-  def app_details_table(app)
-    add_metadata_to_app(app)
-    hr
-    puts "Found ".colorize(:green) + "#{app.name.strip.colorize(:white)}" + " in the ".colorize(:green) + "#{app.category.colorize(:white)}" + " category. ".colorize(:green)
-    print "#{app.description.colorize(:white)}" + " - ".colorize(:green)
-    puts "#{app.overall_rating.colorize(:white)}"
-    puts "App URL: ".colorize(:green) + "#{app.url.colorize(:white)}"
-    puts "Developer: ".colorize(:green) + "#{app.developer_name.colorize(:white)}" + " (#{app.developer_url})".colorize(:green)
-    puts "Developer Contact: ".colorize(:green) + "#{app.developer_contact}".colorize(:white)
-    puts "The overall sentiment for".colorize(:green) + " #{app.name.colorize(:white)} " + "is ".colorize(:green) + "#{app.overall_sentiment}"
-    trending_sentiment(app) unless app.app_reviews.empty?
-  end
-
-  def display_app_reviews(app)
-    add_reviews_to_app(app)
-    total_reviews = app.total_review_count
-    puts "#{app.name}'s Latest Reviews:".colorize(:light_green)
-    hr
-    app.app_reviews.each_with_index do |review, index|
-      puts "##{(index + 1)}. ".colorize(:yellow) + "#{review.title.split.map(&:capitalize).join(' ').colorize(:green)} - #{review.rating}".colorize(:green)
-      puts "Reviewed on #{review.date}".colorize(:green)
-      puts "#{review.body}".colorize(:white)
-      hr unless review == app.app_reviews.last
+  def save_reviews_to_csv(app)
+    CSV.open("outputs/#{app.name}_reviews.csv", "w") do |csv|
+      csv << ["Store Name", "Rating", "Date", "Body", "Location", "Time Spent with App"]
+      app.app_reviews.each do |review|
+        csv << [review.store_name, review.rating, review.date, review.body, review.location, review.time_spent_with_app]
+      end
     end
-  end
-
-  def scrape_and_create_apps
-    app_array = AppListScraper.scrape_shopify_apps
-    ShopifyApp.create_from_collection(app_array)
-  end
-
-  def add_reviews_to_app(app)
-    review_array = AppReviewScraper.scrape_app_reviews(app)
-    AppReview.create_from_collection(review_array, app)
-    app.app_reviews << review_array
-  end
-
-  def add_metadata_to_app(app)
-    metadata = AppReviewScraper.scrape_app_metadata(app)
+    puts "Reviews saved to #{app.name}_reviews.csv".colorize(:green)
   end
 
   def goodbye
     puts "Closing CLI."
+  end
+end
+
+class AppReview
+  attr_accessor :store_name, :rating, :date, :body, :location, :time_spent_with_app
+
+  def initialize(review_attributes)
+    @store_name = review_attributes[:store_name]
+    @rating = review_attributes[:rating]
+    @date = review_attributes[:date]
+    @body = review_attributes[:body]
+    @location = review_attributes[:location]
+    @time_spent_with_app = review_attributes[:time_spent_with_app]
+  end
+end
+
+class ShopifyApp
+  attr_accessor :name, :url, :review_summary, :app_reviews
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @url = attributes[:url]
+    @review_summary = attributes[:review_summary]
+    @app_reviews = []
   end
 end
